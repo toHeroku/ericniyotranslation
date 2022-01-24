f41d8ea19fec8af425d2a63d717c1997fb2d0f3d
ALBERT44-T <71137504+ALBERT44-T@users.noreply.github.com>
2022-01-24 22:42:30 +0200

    Update Noun.php

--- Noun.php
+++ Noun.php
@@ -55,7 +55,16 @@
   </html>
 
 <?php
-$con= new mysqli("localhost","root","","PHP");
+<?php
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+$db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
 $query ="SELECT Kinyarwanda FROM noun_translation ORDER BY RAND() LIMIT 1";
     $word = filter_input(INPUT_POST, 'var');
     
