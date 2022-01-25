c33e2bde222379dba3034deb18e10acba755a14a
ALBERT44-T <71137504+ALBERT44-T@users.noreply.github.com>
2022-01-25 14:05:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -56,18 +56,7 @@
   </html>
 
 <?php
-     <?php
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-?>
+  include'connection.php'
 
 $query ="SELECT Kinyarwanda FROM noun_translation ORDER BY RAND() LIMIT 1";
     $word = filter_input(INPUT_POST, 'var');
