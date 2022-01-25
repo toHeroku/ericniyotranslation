6eea975daace77295d5abd32aa0d86f719c32a23
ALBERT44-T <71137504+ALBERT44-T@users.noreply.github.com>
2022-01-25 15:22:17 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,5 +6,5 @@ $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
-$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
